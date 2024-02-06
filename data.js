0f6ebbbe0850b8fd90ea1b15f890cc01de2a3279
md-whahid <mdislamwhahid@gmail.com>
2024-02-06 22:01:17 +0600

    add jeson file

--- data.js
+++ data.js
@@ -1,5 +1,11 @@
-export { squer };
+// export { squer };
 
-function squer(color, id, piece) {
-  return { color, id, piece };
-}
+// function squer(color, id, piece) {
+//   return { color, id, piece };
+// }
+const prompt = require("prompt-sync")();
+const nam = prompt("What's your name: ");
+console.log("hello", nam);
+const age = prompt("What's your age: ");
+console.log("fine", age);
+alert("Never Gonna Give You Up");
